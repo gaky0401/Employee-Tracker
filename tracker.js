77d5f282e90deaa95985e8291253a612d0c3433b
Kyle Galer <chembot81@hotmail.com>
2021-01-03 18:55:10 -0600

    update tracker options

---
+++ tracker.js
@@ -0,0 +1,7 @@
+const connection = require ("./connections");
+const db = require("./employee.sql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const mysql = require("mysql");
+
+
